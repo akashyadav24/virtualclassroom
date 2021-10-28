b0521b8af66a2973f642d3c208b52e11d7ba2708
akash yadav <akashyadavmrt24@gmail.com>
2021-10-28 15:38:30 +0530

    done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use("/classroom/tutor", tutorRouter);
 app.use("/classroom/student", studentRouter);
-port = process.env.APP_PORT || 5000;
+port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Server is running on PORT : ", port);
 });
